042bf8031f1e72c1dd3330a866a3041867697f4b
mayconBertucci <gekesonmaycon@hotmail.com>
2021-03-15 09:55:08 +0100

    Banco de dados H2 e criação automática de base de dados

---
+++ src/main/java/com/mayconbertucci/mc/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.mayconbertucci.mc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mayconbertucci.mc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {}
---
+++ src/main/java/com/mayconbertucci/mc/service/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.mayconbertucci.mc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mayconbertucci.mc.domain.Categoria;
+import com.mayconbertucci.mc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
