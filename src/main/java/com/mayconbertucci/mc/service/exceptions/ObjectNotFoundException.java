ae2fc7fe952f819350bffe46c8fa28ccc943be6f
mayconBertucci <gekesonmaycon@hotmail.com>
2021-03-15 12:11:24 +0100

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/mayconbertucci/mc/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.mayconbertucci.mc.service.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
